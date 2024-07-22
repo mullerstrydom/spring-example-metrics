c75c4eff1af5cd9c23735c33cdbdda1cf3972ba6
Muller Strydom <external.muller.strydom@kuehne-nagel.com>
2024-07-23 00:46:49 +0200

    initial commit

---
+++ spring-example-metrics/src/main/java/com/example/spring/springexamplemetrics/SpringExampleMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.springexamplemetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleMetricsApplication.class, args);
+	}
+
+}
---
+++ spring-example-metrics/src/test/java/com/example/spring/springexamplemetrics/SpringExampleMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.springexamplemetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
